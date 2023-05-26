51ec5b74bc967e1e6b4ca7c35d98854e52f34b72
Fabricio Henrique <fabricio.fh086@gmail.com>
2023-05-25 22:50:10 -0300

    feat: filtros e mixins

---
+++ filtros-mixins/src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Filtros e mixins</h1>
+    <hr>
+    <p>{{ cpf | inverter | formataCPF }}</p>
+    <hr>
+    <h2>Frutas</h2>
+    <ul>
+      <li v-for="fruta in frutas" :key="fruta">{{ fruta }}</li>
+    </ul>
+    <input>
+    <hr>
+    <NovoFrutas />
+  </div>
+</template>
+
+<script>
+import FrutasVue from './components/Frutas.vue'
+export default {
+  components: {
+    FrutasVue
+  },
+
+  filters: {
+    formataCPF(valor){
+    const array = valor.split('');
+    array.splice(3, 0, '.');
+    array.splice(7, 0, '.');
+    array.splice(11, 0, '-');
+    return array.join('');
+    }
+  },
+  data() {
+    return{
+      cpf: '44455577700',
+      frutas: ['maçã', 'morango', 'mamão'],
+      fruta: ''
+    }
+  },
+  methods: {
+    add(){
+      this. frutas.push(this.fruta);
+      this.fruta = ''
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ filtros-mixins/src/components/NovoFrutas.vue
@@ -0,0 +1,15 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import NovoFrutas from './components/NovoFrutas';
+import frutas from './mixins/frutas';
+export default {
+    mixins: [frutas]
+}
+</script>
+
+<style>
+
+</style>
